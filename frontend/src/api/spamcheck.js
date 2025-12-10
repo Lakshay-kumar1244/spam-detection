64126c922f9c2b54a59a40e3a2cd10db8911b21b
Lakshay-kumar1244 <littlelucky1244@gmail.com>
2025-12-11 00:54:24 +0530

    Connect frontend to deployed backend

--- frontend/src/api/spamcheck.js
+++ frontend/src/api/spamcheck.js
@@ -1,20 +1,11 @@
-// import axios from "axios";
 
-// export const checkSpam = async (text) => {
-//   const response = await axios.post("http://127.0.0.1:8000/check_spam", {
-//     text: text,
-//   });
-
-//   return response.data;
-// };
 import axios from "axios";
 
 const API_URL = "https://spam-detection-backend-vbzq.onrender.com";
 
 export const checkSpam = async (text) => {
   const response = await axios.post(`${API_URL}/check_spam`, {
-    text,
+    text: text,
   });
-
   return response.data;
 };
